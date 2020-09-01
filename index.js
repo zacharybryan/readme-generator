eb318a00e079ffbef2b71d74c556033e61626694
Zach Bryan <zacharybryan@icloud.com>
2020-08-31 18:15:53 -0700

    added info from assignment

--- index.js
+++ index.js
@@ -36,3 +36,20 @@ function promptUser() {
       }
     ]);
   }
+
+  // array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
