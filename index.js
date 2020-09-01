1809a24120ef22a223a1affca76d87380270640f
Zach Bryan <zacharybryan@icloud.com>
2020-08-31 18:12:25 -0700

    added basic prompts

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
+
+function promptUser() {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+      },
+      {
+        type: "input",
+        name: "location",
+        message: "Where are you from?"
+      },
+      {
+        type: "input",
+        name: "hobby",
+        message: "What is your favorite hobby?"
+      },
+      {
+        type: "input",
+        name: "food",
+        message: "What is your favorite food?"
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "Enter your GitHub Username"
+      },
+      {
+        type: "list",
+        name: "licence",
+        message: "Enter your LinkedIn URL."
+      }
+    ]);
+  }
